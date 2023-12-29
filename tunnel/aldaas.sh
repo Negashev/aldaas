34271224954d722f22c8a251f1054e4c2a998900
Negash <i@negash.ru>
2023-12-29 13:24:32 +0000

    Add outputs

--- tunnel/aldaas.sh
+++ tunnel/aldaas.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-ALDAAS_NAMESPACE=aldaas
 ALDAAS_AWT=aldaas-chart
 
-argo -n $ALDAAS_NAMESPACE submit --from workflowtemplate/$ALDAAS_AWT --watch
+argo submit --from workflowtemplate/$ALDAAS_AWT --watch

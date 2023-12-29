dbf72e0ec33d59bfdbcf1314aed7c5be849fed8a
Negash <i@negash.ru>
2023-12-29 16:13:03 +0000

    aldaas on client side

--- tunnel/aldaas.sh
+++ tunnel/aldaas.sh
@@ -1,4 +1,28 @@
 #!/bin/sh
-ALDAAS_AWT=aldaas-chart
+ALDAAS_PROTOCOL="${ALDAAS_PROTOCOL:-ws}"
+ALDAAS_SERVER="${ALDAAS_SERVER:-$ARGO_SERVER}"
+ALDAAS_PORT="${ALDAAS_PORT:-5432}"
 
-argo submit --from workflowtemplate/$ALDAAS_AWT --watch
+aldaas_name=""
+# check save
+FILE=/tmp/aldaas
+if [ -f "$FILE" ]; then
+    echo "save file exists."
+    aldaas_name=`cat $FILE`
+    # check wf exist
+    if ! argo get $aldaas_name; then
+        aldaas_name=""
+        echo "Not found $aldaas_name"
+    else
+        echo "Use saved $aldaas_name"
+    fi
+fi
+
+# if no save create new aldaas wf
+if [[ -z $aldaas_name ]]; then
+    aldaas_name=`argo submit --from workflowtemplate/$ALDAAS_NAME -o name`
+    argo watch $aldaas_name
+    echo $aldaas_name > $FILE
+fi
+
+tcp-over-websocket  client -listen_tcp 0.0.0.0:$ALDAAS_PORT -connect_ws "$ALDAAS_PROTOCOL://$ALDAAS_SERVER/$ALDAAS_NAME/$ALDAAS_TOKEN/$aldaas_name"
